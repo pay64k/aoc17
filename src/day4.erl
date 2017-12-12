8b8e5de955f88a134c26bb10d5c2e50b01257a23
jakubs <jakub.slupecki@motorolasolutions.com>
2017-12-12 15:33:17 +0100

    day4a done

---
+++ src/day4.erl
@@ -0,0 +1,47 @@
+-module(day4).
+-compile([export_all]).
+
+
+run_day4a() ->
+  Contents = readlines("input/day4_input"),
+  Input = format_contents(Contents),
+  Result = [{maps:size(make_map(X)), length(X)} || X <- Input],
+  Amount = [1 || {X, Y} <- Result , X==Y],
+  io:format("Result: ~p~n", [lists:foldl(fun(X, Sum) -> X + Sum end, 0, Amount)]).
+
+make_map(List) ->
+  make_map(List, maps:new()).
+
+make_map([], Acc) ->
+  Acc;
+
+make_map([H | T], Acc) ->
+  make_map(T, maps:put(H, 0, Acc)).
+
+%%---------------------------------
+
+run_day4b() ->
+  run_day4a(),
+  Contents = readlines("input/day4_input"),
+  Input = format_contents(Contents),
+  io:format("Input: ~p~n", [Input]),
+  ok.
+
+%%---------------------------------
+
+format_contents(Input) ->
+  Lines = string:split("abcde fghij\nabcde xyz ecdab\noiii ioii iioi iiio", "\n", all),
+  Rows = [string:split(N, " ", all) || N <- Lines],
+  [Entry || Entry <- Rows].
+
+readlines(FileName) ->
+  {ok, Device} = file:open(FileName, [read]),
+  try get_all_lines(Device)
+  after file:close(Device)
+  end.
+
+get_all_lines(Device) ->
+  case io:get_line(Device, "") of
+    eof -> [];
+    Line -> Line ++ get_all_lines(Device)
+  end.
